13fed8b4f51b576984a50bd367f76715b5f712f8
abuabdullah23 <abuobaidazayed@gmail.com>
2023-06-12 20:03:00 +0600

    parallax bg added and new button style

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -5,22 +5,24 @@ import './Featured.css';
 
 const Featured = () => {
     return (
-        <div className='featured-bg md:py-10 text-white mb-10'>
-            <SectionTitle
-                subHeading={"Check it out"}
-                heading={"FROM OUR MENU"}
-            ></SectionTitle>
+        <div className='featured-bg bg-fixed text-white mb-10'>
+            <div className='w-full h-full bg-black bg-opacity-60 py-16'>
+                <SectionTitle
+                    subHeading={"Check it out"}
+                    heading={"FROM OUR MENU"}
+                ></SectionTitle>
 
-            <div className='flex justify-center items-center'>
-                <div className='md:flex gap-10 justify-center items-center w-3/4'>
-                    <div>
-                        <img className='3/4' src={featuredImg} alt="" />
-                    </div>
-                    <div>
-                        <p>March 20, 2023</p>
-                        <p>WHERE CAN I GET SOME?</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptate facere, deserunt dolores maiores quod nobis quas quasi. Eaque repellat recusandae ad laudantium tempore consequatur consequuntur omnis ullam maxime tenetur.</p>
-                        <button>Read More</button>
+                <div className='flex justify-center items-center'>
+                    <div className='md:flex gap-10 justify-center items-center w-3/4 '>
+                        <div>
+                            <img className='3/4' src={featuredImg} alt="" />
+                        </div>
+                        <div>
+                            <p>March 20, 2023</p>
+                            <p>WHERE CAN I GET SOME?</p>
+                            <p className='text-xs'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptate facere, deserunt dolores maiores quod nobis quas quasi. Eaque repellat recusandae ad laudantium tempore consequatur consequuntur omnis ullam maxime tenetur.</p>
+                            <button className='btn btn-outline border-0 border-b-4 text-white'>Read More</button>
+                        </div>
                     </div>
                 </div>
             </div>
