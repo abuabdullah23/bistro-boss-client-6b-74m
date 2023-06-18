e9f793d6648a8ff61c5cfbb7aff5750b3e16a814
abuabdullah23 <abuobaidazayed@gmail.com>
2023-06-19 00:10:12 +0600

    Jwt Token authorize with axios

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -6,11 +6,16 @@ import { AuthContext } from '../provider/AuthProvider';
 
 const useCart = email => {
     const { user } = useContext(AuthContext);
+    const token = localStorage.getItem('access-token')
 
     const { refetch, data: cart = [] } = useQuery({
         queryKey: ['carts', user?.email],
         queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/carts?email=${user.email}`)
+            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`, {
+                headers: {
+                    authorization : `bearer ${token}`
+                }
+            })
             return res.json();
         },
     })
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -4,6 +4,7 @@ import { createContext } from 'react';
 export const AuthContext = createContext(null);
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from '../firebase/firebase.config';
+import axios from 'axios';
 
 const auth = getAuth(app);
 
@@ -49,7 +50,19 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             console.log(currentUser)
+
+            // jwt token with axios data fetching
+            if (currentUser) {
+                axios.post('http://localhost:5000/jwt', { email: currentUser.email })
+                    .then(data => {
+                        console.log(data.data.token)
+                        localStorage.setItem('access-token', data.data.token)
+                    })
+            } else {
+                localStorage.removeItem('access-token');
+            }
             setLoading(false)
+
         })
         return () => {
             return unsubscribe();
