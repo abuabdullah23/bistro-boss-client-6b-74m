eb68669321ab8ed12d0c228120b0db42d34d0fa0
abuabdullah23 <abuobaidazayed@gmail.com>
2023-06-21 18:51:41 +0600

    Try to secure update item method

--- src/pages/Dashboard/Admin/UpdateItem/UpdateItem.jsx
+++ src/pages/Dashboard/Admin/UpdateItem/UpdateItem.jsx
@@ -4,47 +4,23 @@ import { useLoaderData } from 'react-router-dom';
 import SectionTitle from '../../../../components/SectionTitle';
 import { useForm } from 'react-hook-form';
 import Swal from 'sweetalert2';
+import useAxiosSecure from '../../../../hooks/useAxiosSecure';
 
 const UpdateItem = () => {
     const { register, handleSubmit } = useForm();
     const item = useLoaderData();
+    const [axiosSecure] = useAxiosSecure();
 
     const onSubmit = data => {
         // update single menu Item
         const {name, price, category, recipe} = data;
         const updateItem = {name, price: parseInt(price), category: category.toLowerCase(), recipe}
-        fetch(`http://localhost:5000/dashboard/update-menu/${item._id}`, {
-            method: "PUT",
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(updateItem)
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                if (data.modifiedCount > 0) {
-                    // ==================== Success Alert
-                    Swal.fire({
-                        title: 'Updated!',
-                        text: 'Successfully updated this Item information!',
-                        icon: 'success',
-                        confirmButtonText: 'ok'
-                    })
-                    // ==================== Success Alert 
-                } else {
-                    // ==================== Error Alert
-                    Swal.fire({
-                        title: 'Not Updated Anything!',
-                        text: `Please change value of this item information!`,
-                        icon: 'error',
-                        confirmButtonText: 'Ok'
-                    })
-                    // ==================== Error Alert
-                }
-            })
-        console.log(data)
-    };
+
+        axiosSecure.put('/dashboard/update-menu', updateItem)
+        .then(res=>{
+            console.log(res.data);
+        })    
+    }
 
     return (
         <div>
@@ -105,3 +81,38 @@ const UpdateItem = () => {
 };
 
 export default UpdateItem;
+
+// fetch(`http://localhost:5000/dashboard/update-menu/${item._id}`, {
+//             method: "PUT",
+//             headers: {
+//                 'content-type': 'application/json'
+//             },
+//             body: JSON.stringify(updateItem)
+//         })
+//             .then(res => res.json())
+//             .then(data => {
+//                 console.log(data)
+                
+//             })
+//         console.log(data)
+//     };
+
+// if (data.modifiedCount > 0) {
+//     // ==================== Success Alert
+//     Swal.fire({
+//         title: 'Updated!',
+//         text: 'Successfully updated this Item information!',
+//         icon: 'success',
+//         confirmButtonText: 'ok'
+//     })
+//     // ==================== Success Alert 
+// } else {
+//     // ==================== Error Alert
+//     Swal.fire({
+//         title: 'Not Updated Anything!',
+//         text: `Please change value of this item information!`,
+//         icon: 'error',
+//         confirmButtonText: 'Ok'
+//     })
+//     // ==================== Error Alert
+// }
