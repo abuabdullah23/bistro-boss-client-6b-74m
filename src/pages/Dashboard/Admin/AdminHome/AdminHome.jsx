45ac4adc32c6a3a6dda7dfc1702e3da59abab20a
abuabdullah23 <abuobaidazayed@gmail.com>
2023-06-22 06:25:16 +0600

    Admin home page data load use stats

--- src/pages/Dashboard/Admin/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/Admin/AdminHome/AdminHome.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { FaCarAlt, FaUserAlt, FaWallet } from 'react-icons/fa';
 import { SiCodechef } from "react-icons/si";
-import useMenu from '../../../../hooks/useMenu';
 import useAxiosSecure from '../../../../hooks/useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
 import useAuth from '../../../../hooks/useAuth';
@@ -10,22 +9,23 @@ const AdminHome = () => {
     const [axiosSecure] = useAxiosSecure();
     const {user} = useAuth();
 
-    const { data: users = [], refetch } = useQuery(['users'], async () => {
-        const res = await axiosSecure.get('/users')
-        return res.data;
+    const {data: stats= {}} = useQuery({
+        queryKey:['admin-stats'],
+        queryFn: async() => {
+            const res = await axiosSecure('/admin-stats');
+            return res.data;
+        }
     })
-    const [ menu ] = useMenu();
-    console.log(menu)
 
     return (
         <div>
-            <h2 className='text-3xl mb-5'>HI, Welcome Back, <span className='font-bold'>{user?.displayName}</span>!</h2>
+            <h2 className='text-3xl drop-shadow-md mb-5'>HI, <span className='font-bold'>{user?.displayName}</span>!</h2>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5'>
                 {/* revenue */}
                 <div className='bg-gradient-to-r from-[#BB34F5] to-[#FCDBFF] rounded-md py-5 px-5 flex gap-6 items-center justify-center text-white'>
                     <FaWallet className='w-8 h-8' />
                     <div>
-                        <h3 className='text-3xl font-bold'>1000</h3>
+                        <h3 className='text-3xl drop-shadow-md font-bold'>$ {stats.revenue}</h3>
                         <p className='text-lg'>Revenue</p>
                     </div>
                 </div>
@@ -34,7 +34,7 @@ const AdminHome = () => {
                 <div className='bg-gradient-to-r from-[#D3A256] to-[#FDE8C0] rounded-md py-5 px-5 flex gap-6 items-center justify-center text-white'>
                     <FaUserAlt className='w-8 h-8' />
                     <div>
-                        <h3 className='text-3xl font-bold'>{users.length}</h3>
+                        <h3 className='text-3xl drop-shadow-md font-bold'>{stats.users}</h3>
                         <p className='text-lg'>Customers</p>
                     </div>
                 </div>
@@ -43,7 +43,7 @@ const AdminHome = () => {
                 <div className='bg-gradient-to-r from-[#FE4880] to-[#FECDE9] rounded-md py-5 px-5 flex gap-6 items-center justify-center text-white'>
                     <SiCodechef className='w-8 h-8' />
                     <div>
-                        <h3 className='text-3xl font-bold'>{menu.length}</h3>
+                        <h3 className='text-3xl drop-shadow-md font-bold'>{stats.products}</h3>
                         <p className='text-lg'>Products</p>
                     </div>
                 </div>
@@ -52,7 +52,7 @@ const AdminHome = () => {
                 <div className='bg-gradient-to-r from-[#6AAEFF] to-[#B6F7FF] rounded-md py-5 px-5 flex gap-6 items-center justify-center text-white'>
                     <FaCarAlt className='w-8 h-8' />
                     <div>
-                        <h3 className='text-3xl font-bold'>500</h3>
+                        <h3 className='text-3xl drop-shadow-md font-bold'>{stats.orders}</h3>
                         <p className='text-lg'>Orders</p>
                     </div>
                 </div>
